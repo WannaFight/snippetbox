f1dc8e860ec107e96e66bf1a681cdfb15610895e
Ivan Trushin <itrushin99@gmail.com>
2024-03-16 15:58:54 +0300

    feat: add static

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 	mux.HandleFunc("/", home) // subtree path like "/**" or "/static/**"
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
@@ -14,4 +18,16 @@ func main() {
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
+}
+
+// Disable looking up static dirs
+func neuter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasSuffix(r.URL.Path, "/") {
+			http.NotFound(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
