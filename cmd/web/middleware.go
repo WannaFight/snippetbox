a29400ccd8d8ac29ed7d71e467a23a1be7d284b9
Ivan Trushin <itrushin99@gmail.com>
2024-03-21 23:30:19 +0300

    chore: add csrf protection

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+
+	"github.com/justinas/nosurf"
 )
 
 // Disable looking up static dirs directly
@@ -61,4 +63,14 @@ func (app *application) requireAuthentication(next http.Handler) http.Handler {
 		w.Header().Add("Cache-Control", "no-store")
 		next.ServeHTTP(w, r)
 	})
+}
+
+func noSurf(next http.Handler) http.Handler {
+	csrfHandler := nosurf.New(next)
+	csrfHandler.SetBaseCookie(http.Cookie{
+		HttpOnly: true,
+		Path:     "/",
+		Secure:   true,
+	})
+	return csrfHandler
 }
